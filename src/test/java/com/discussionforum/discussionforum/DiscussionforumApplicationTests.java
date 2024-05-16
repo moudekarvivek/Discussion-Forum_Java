d38a434dd6a3623cb69417ccf57eb3c5e32bf032
moudekarvivek <moudekarvivek@gmail.com>
2024-05-16 23:09:32 +0530

    Folder Structure and Dependency

---
+++ src/test/java/com/discussionforum/discussionforum/DiscussionforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discussionforum.discussionforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
